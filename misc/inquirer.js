489e6c1758eaa0581711f2e81573cefc1c9dc4f0
adso023 <43103279+adso023@users.noreply.github.com>
2020-03-25 22:32:55 -0400

    Updated question prompts

--- misc/inquirer.js
+++ misc/inquirer.js
@@ -67,18 +67,44 @@ module.exports = {
         return inquirer.prompt(question);
     },
     askSearchParam: () => {
-        const question = {
-            name: 'searchparam',
-            type: 'input',
-            message: 'Please enter a title to search: ',
-            validate: function(value) {
-                if(value.length){
-                    return true;
-                } else {
-                    return "Enter a title to search."
+        const question = [
+            {
+                name: 'title',
+                type: 'input',
+                message: 'Please enter a title to search: ',
+                validate: function(value) {
+                    if(value.length){
+                        return true;
+                    } else {
+                        return "Enter a title to search."
+                    }
+                }
+            },
+            {
+                name: 'type',
+                type: 'input',
+                message: 'Type for search [movie, series, episode]: ',
+                validate: function(value) {
+                    if(value.length && (value === 'movie' || value === 'series' || value === 'episode')) {
+                        return true;
+                    } else {
+                        return 'Invalid type search [movie, series, episode].'
+                    }
+                }
+            },
+            {
+                name: 'page',
+                type: 'input',
+                message: 'Enter page number [1,100]: ',
+                validate: function(value) {
+                    if (value.length && value >= 1 && value.length <= 100) {
+                        return true;
+                    } else {
+                        return "Enter valid page number [1,100]."
+                    }
                 }
             }
-        };
+        ];
 
         return inquirer.prompt(question);
     }
